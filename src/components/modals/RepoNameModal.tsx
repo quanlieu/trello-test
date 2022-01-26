b36f764ac3510e73164d4c9f32ab08ac6ff9793a
Quan Lieu <lieuchiquan@gmail.com>
2022-01-26 16:25:00 +0700

    Change folder

---
+++ src/components/modals/RepoNameModal.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+export interface IProps {
+  show: boolean;
+  name?: string;
+  onSubmit: (repoName: string) => void;
+  onClose: () => void;
+}
+
+function RepoNameModal(props: IProps) {
+  const { show, name, onSubmit, onClose } = props;
+  const [repoName, setRepoName] = useState(name ?? '');
+  const handleChangeName = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setRepoName(e.currentTarget.value);
+  const handleSubmit = () => {
+    onSubmit(repoName);
+    onClose();
+  };
+
+  return (
+    <Modal show={show} onHide={onClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Repo information</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3" controlId="repoName">
+            <Form.Label>Repo name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter a name"
+              onChange={handleChangeName}
+              value={repoName}
+              data-testid="repo-name"
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onClose} data-testid="close-btn">
+          Close
+        </Button>
+        <Button
+          variant="primary"
+          onClick={handleSubmit}
+          data-testid="submit-btn"
+        >
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default RepoNameModal;
